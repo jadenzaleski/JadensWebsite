87a0bc5ad03d485b9abdde79bbe80fc6179cff52
Jaden Zaleski <jadenzaleski@icloud.com>
2022-12-19 23:24:31 -0500

    login

--- authenticate.php
+++ authenticate.php
@@ -1,5 +1,17 @@
 <?php
-include 'connect.php';
+
+session_start();
+// Change this to your connection info.
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = 'august30';
+$DATABASE_NAME = 'phplogin';
+// Try and connect using the info above.
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if ( mysqli_connect_errno() ) {
+    // If there is an error with the connection, stop the script and display the error.
+    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
 
 if ( !isset($_POST['username'], $_POST['password']) ) {
     // Could not get the data that should have been sent.
