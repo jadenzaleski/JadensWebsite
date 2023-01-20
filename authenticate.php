f020518532c938db00aa774222a0d33a51bccb3a
Jaden Zaleski <jadenzaleski@icloud.com>
2023-01-20 16:28:38 -0500

    auth

--- authenticate.php
+++ authenticate.php
@@ -12,6 +12,8 @@ $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_
 if ( mysqli_connect_errno() ) {
     // If there is an error with the connection, stop the script and display the error.
     echo 'Failed to connect to MySQL: ' . mysqli_connect_error();
+} else {
+    echo 'success';
 }
 
 //if ( !isset($_POST['username'], $_POST['password']) ) {
