d582f5cf5df6128f33dae51d9d5126c3c00212be
Jaden Zaleski <jadenzaleski@icloud.com>
2023-03-29 19:39:41 -0400

    working on #6

--- index.php
+++ index.php
@@ -120,9 +120,18 @@ session_start();
                               d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
                     </svg>
                 </button>
-                <div class="dropdown-menu dropdown-menu-end p-4" style="width: 300px !important;" id="profile">
-<!--                    TODO finish sign in drop down-->
+                <div class="dropdown-menu dropdown-menu-end p-4 text-center" style="width: 300px !important;">
+                    <h6>test name</h6>
                     <hr class="dropdown-divider">
+                    <div class="text-start">
+                        <p>Username: tester</p>
+                        <p>Email: test@testing.comocom</p>
+                        <p>User since: 2023-03-27 23:08:14</p>
+                    </div>
+                    <button class="btn w-100" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right mb-1" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+                            <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                        </svg>Logout</button>
                 </div>
             </div>
             <?php } else { ?>
@@ -302,7 +311,7 @@ session_start();
             <div class="col-lg-6 col-md-8 mx-auto">
                 <h1 class="fw-light">Hello!</h1>
                 <p class="lead text-muted">have responsive picture and general explanation with details on what the site
-                    is. include linkedin/gihub and also refer to info tab. talk about general site navigation and login.
+                    is. include LinkedIn/GitHub and also refer to info tab. talk about general site navigation and login.
                 </p>
             </div>
         </div>
