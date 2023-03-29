485d8d274296b2b17d9f1dc19c1ad39957f651f0
Jaden Zaleski <jadenzaleski@icloud.com>
2023-03-28 20:56:30 -0400

    working on #6

--- index.php
+++ index.php
@@ -107,12 +107,21 @@ session_start();
                 <?php
                 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) { ?>
                     Hello, <?php echo $_SESSION['firstname'] ?>
+                    <a type="button" href="profile.php" target="_blank"
+                            class="btn pt-1 ms-md-0 mb-2 mb-md-0 float-end caret-off border-0"
+                            data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                         class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                        <path fill-rule="evenodd"
+                              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                    </svg>
+                </a>
                 <?php } else { ?>
                     Login / Sign Up
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                          class="bi bi-chevron-right m-0" viewBox="0 0 16 16"><path fill-rule="evenodd"
                                                                                    d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/></svg>
-                <?php } ?>
             </span>
             <div class="dropdown">
                 <button type="button"
@@ -183,6 +192,7 @@ session_start();
                             password?</a></p>
                 </div>
             </div>
+            <?php } ?>
         </div>
     </div>
 </nav>
