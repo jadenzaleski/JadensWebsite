c41d6f839d2f26e55da6de7392b1f1b33ea2de7b
Jaden Zaleski <jadenzaleski@icloud.com>
2023-03-28 21:23:07 -0400

    working on #6

--- index.php
+++ index.php
@@ -120,7 +120,7 @@ session_start();
                               d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
                     </svg>
                 </button>
-                <div class="dropdown-menu dropdown-menu-end p-4" id="profile">
+                <div class="dropdown-menu dropdown-menu-end p-4 w-100" id="profile">
                     <p>
                         Some example text that's free-flowing within the dropdown menu.
                     </p>
