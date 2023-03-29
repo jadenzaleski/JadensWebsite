b12c42088ea092f834747d0f0a40088962f44e38
Jaden Zaleski <jadenzaleski@icloud.com>
2023-03-29 19:45:26 -0400

    working on #6

--- index.php
+++ index.php
@@ -112,7 +112,7 @@ session_start();
             <div class="dropdown">
                 <button type="button"
                         class="btn login-button pt-1 ms-md-0 mb-2 mb-md-0 float-end dropdown-toggle caret-off border-0"
-                        data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
+                        data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="true">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
                          class="bi bi-person-circle" viewBox="0 0 16 16">
                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
@@ -121,14 +121,14 @@ session_start();
                     </svg>
                 </button>
                 <div class="dropdown-menu dropdown-menu-end p-4 text-center" style="width: 300px !important;">
-                    <h6>test name</h6>
+                    <h5>test name</h5>
                     <hr class="dropdown-divider">
                     <div class="text-start">
-                        <p>Username: tester</p>
-                        <p>Email: test@testing.comocom</p>
-                        <p>User since: 2023-03-27 23:08:14</p>
+                        <p class="mb-1">Username: tester</p>
+                        <p class="mb-1">Email: test@testing.comocom</p>
+                        <p class="mb-1">User since: 2023-03-27 23:08:14</p>
                     </div>
-                    <button class="btn w-100" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right mb-1" viewBox="0 0 16 16">
+                    <button class="btn w-100" id="logout" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right mb-1 me-1" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
                             <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
                         </svg>Logout</button>
