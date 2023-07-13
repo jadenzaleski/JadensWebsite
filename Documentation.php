b1a7663cffbe065ab1c7408676cf7e2fda8c7549
jaden <jadenzaleski@icloud.com>
2023-07-12 23:13:55 -0400

    updated Documentation.php

--- Documentation.php
+++ Documentation.php
@@ -7,7 +7,7 @@ session_start();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Info</title>
+    <title>Documentation</title>
     <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
